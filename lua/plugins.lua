8fa4934f5a45eed41f9b60475c9ff0386cb2b5fb
Rob Stevenson <this.rob@protonmail.com>
2024-04-06 15:07:55 +0100

    Fix formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,9 +20,9 @@ require('nvim-treesitter.configs').setup {
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting=false,
+    additional_vim_regex_highlighting = false,
   },
-  indent = { enable = true }, 
+  indent = { enable = true },
   endwise = { enable = false },
   rainbow = {
     enable = true,
