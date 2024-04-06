d63f398b0daab2c020b604c87431da285913d54d
Rob Stevenson <this.rob@protonmail.com>
2024-04-06 15:07:32 +0100

    Add telescope keymap to search Ruby project files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,7 +32,15 @@ require('nvim-treesitter.configs').setup {
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fra', function()
+  builtin.find_files({search_dirs = {'app'}})
+end, {})
+vim.keymap.set('n', '<leader>frt', function()
+  builtin.find_files({search_dirs = {'test'}})
+end, {})
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({hidden = true})
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
