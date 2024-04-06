c408b199aacc99745ae916eb212ff189d3f51f88
Rob Stevenson <this.rob@protonmail.com>
2024-04-06 15:06:56 +0100

    Set up for Rust

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "ruby", "markdown" },
+  ensure_installed = { "lua", "ruby", "markdown", "rust", "toml" },
   auto_install = true,
   highlight = {
     enable = true,
@@ -178,3 +178,33 @@ vim.keymap.set('n', '<Leader>mw', function()
 end)
 
 vim.g['test#strategy'] = 'neovim'
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
