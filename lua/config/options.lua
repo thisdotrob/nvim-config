d21f7aa1ccb32f415e8f021c06c6a07157d7657d
Rob Stevenson <this.rob@protonmail.com>
2024-10-20 11:00:09 +0100

    Set indent options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@ vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.tabstop = 2 -- Number of spaces tabs count for
 vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.autoindent = true -- each new line inherits the indentation from the previous line
+vim.opt.smartindent = true -- automatically adjust the indentation when editing certain file types
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.shiftround = true -- align the indented lines to a multiple of the shift width
