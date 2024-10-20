f51cc8696a73818e02569b95da99294dcf1403b1
Rob Stevenson <this.rob@protonmail.com>
2024-10-20 11:51:39 +0100

    Set find files keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,3 @@
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files (root dir)
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>") -- recently opened files
 
