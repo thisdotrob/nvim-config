a59aa29a690e99db429c41a44e8d86d3914bd281
Rob Stevenson <this.rob@protonmail.com>
2024-10-20 10:55:27 +0100

    Set tabs vs spaces options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,5 @@
 vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.list = true -- Show some invisible characters (tabs...
