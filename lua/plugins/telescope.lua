1350dd1ec5998e1b392a27f5af324154eff6338f
Rob Stevenson <this.rob@protonmail.com>
2024-10-21 21:59:25 +0100

    Fix formatting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = '0.1.8', -- telescope readme says to pin it to this version
+  tag = "0.1.8", -- telescope readme says to pin it to this version
   dependencies = {
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope-fzf-native.nvim" },
