72b5b7aecb1a4e50fa50e6d755f344040c10f5e1
Rob Stevenson <this.rob@protonmail.com>
2024-10-24 11:52:30 +0100

    Set up linting for SQL

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,8 +1,15 @@
 return {
   "mfussenegger/nvim-lint",
   config = function(_, opts)
-    require("lint").linters_by_ft = {
+    local lint = require("lint")
+
+    -- https://github.com/mfussenegger/nvim-lint/issues/469#issuecomment-2403282672
+    lint.linters.sqlfluff.args = { "lint", "--format=json", "--dialect=redshift", "-" }
+    lint.linters.sqlfluff.stdin = true
+
+    lint.linters_by_ft = {
       ruby = { "rubocop" },
+      sql = { "sqlfluff" },
     }
   end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   main = "nvim-treesitter.configs",
   opts = {
-    ensure_installed = { "ruby" },
+    ensure_installed = { "ruby", "sql" },
     highlight = { enable = true },
     indent = { enable = true },
   },
