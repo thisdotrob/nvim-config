9768178679da380c7bc4606cdb5e5195a530ac1c
Rob Stevenson <this.rob@protonmail.com>
2024-12-06 18:29:55 +0000

    Set up keymaps to send telescope results to quickfix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,4 +6,19 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim" },
   },
   cmd = "Telescope", -- only load the plugin when the Telescope command is first used
+  config = function()
+    local actions = require("telescope.actions")
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+          },
+          n = {
+            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+          },
+        },
+      },
+    })
+  end,
 }
